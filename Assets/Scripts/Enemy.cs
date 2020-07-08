61103eea61c1140dd14d9b77aaad8f497444bcd8
Francesco Spizzirri <fra.spizzirri@gmail.com>
2020-07-08 10:21:27 +0200

    Enemy - Added enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public float life;
+
+    public float movingSpeed;
+
+    private Rigidbody rigidbody;
+
+    private GameObject target;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        FollowTarget();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -22,6 +39,15 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void FollowTarget()
+    {
+        GameObject target = GameObject.FindGameObjectWithTag("Player");
+
+        transform.LookAt(target.transform);
+
+        rigidbody.AddRelativeForce(Vector3.forward * movingSpeed, ForceMode.Force);
     }
 
     private void OnDestroy()
