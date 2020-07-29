5aca599624651fd8c459bf95afb4ef60fd833345
Francesco Spizzirri <fra.spizzirri@gmail.com>
2020-07-29 11:23:05 +0200

    Added Enemy explosion

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -10,6 +10,18 @@ public class Enemy : MonoBehaviour
 
     public float movingSpeed;
 
+    //explosion
+    public float cubeSize = 0.2f;
+    public int cubesInRow = 5;
+    public float explosionForce = 50f;
+    public float explosionRadius = 4f;
+    public float explosionUpward = 0.4f;
+
+    private Vector3 cubesPivot;
+    private Material myMaterial;
+
+    //end explosion
+
     private new Rigidbody rigidbody;
 
     private GameObject target;
@@ -17,6 +29,8 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        myMaterial = GetComponent<MeshRenderer>().material;
+
         target = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -43,6 +57,7 @@ public class Enemy : MonoBehaviour
         if (lifePoints <= 0)
         {
             player.enemiesCounter++;
+            Explosion();
             Destroy(gameObject);
         }
     }
@@ -59,8 +74,69 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void Explosion()
+    {
+        gameObject.SetActive(false);
+
+        //loop 3 times to create 5x5x5 pieces in x,y,z coordinates
+        for (int x = 0; x < cubesInRow; x++)
+        {
+            for (int y = 0; y < cubesInRow; y++)
+            {
+                for (int z = 0; z < cubesInRow; z++)
+                {
+                    createPiece(x, y, z);
+                }
+            }
+        }
+
+        //get explosion position
+        Vector3 explosionPos = transform.position;
+        //get colliders in that position and radius
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+        //add explosion force to all colliders in that overlap sphere
+        foreach (Collider hit in colliders)
+        {
+            if (hit.CompareTag("CubeExplosion"))
+            {
+                //get rigidbody from collider object
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    //add explosion force to this body with given parameters
+                    rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpward);
+                }
+                Destroy(hit.gameObject, 2f);
+            }
+        }
+    }
+
+    void createPiece(int x, int y, int z)
+    {
+        //create piece
+        GameObject piece;
+        piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+     
+        piece.tag = "CubeExplosion";
+
+        //set piece position and scale
+        piece.transform.position = transform.position + new Vector3(cubeSize * x, cubeSize * y, cubeSize * z) - cubesPivot;
+        piece.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+
+        //add rigidbody and set mass
+        piece.AddComponent<Rigidbody>();
+        piece.GetComponent<Rigidbody>().mass = cubeSize;
+
+        piece.GetComponent<MeshRenderer>().material = myMaterial;
+
+    }
+
     private void OnDestroy()
     {
         Debug.Log("Enemy Killed!");
     }
+
 }
+
+
+
